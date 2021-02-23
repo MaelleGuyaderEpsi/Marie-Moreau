817e400fa1844b6daadea101a06b7f98c8784a0e
DESKTOP-VDT1J7R\maell <maelle.guyader@epsi.fr>
2021-02-23 12:06:43 +0100

    commit - page index + css

---
+++ Maquette 1/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="widht=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./css/base.css" type="text/css" media="screen">
+    </head>
+
+    <body>
+
+        <div class="header">
+            
+            <div class="titre">
+                BOB
+            </div>
+
+            <div class="description">
+                 L'application de soutien
+            </div>
+
+            <div class="buttonConnexion">
+                <a id="buttonConnexionLien" href="./connexion.php">Connexion</a>
+            </div>
+
+        </div>
+
+    </body>
+</html>
